335930d1a0691bb2cf2d02687d20eeeffe6d1665
Dylan Heinrich <harly13@live.com>
2019-02-12 23:20:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,12 @@
 function basicTeenager(age) {
   //check age if is greater to or equal to 13 and age is less then 19
-  if age >= 13 && age <= 19{
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
 }
 
 function teenager(age) {
-  if age >= 13 && age <= 19{
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
   else {
@@ -16,11 +16,11 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if age >= 13 && age <= 19{
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
   //checks to see if age is equal to or less then 12
-  else if age <= 12 {
+  else if (age <= 12) {
     return "You are a kid"
   }
   else {
